1a6a23f938a69a64cd142cf0ecf444fd61c101d5
anelefunani <aneleericfunani@gmail.com>
2016-09-06 16:44:42 +0200

    Add Factorial Project and initial test

---
+++ Factorial/Test/com/britehouse/tdd/FactorialTest.java
@@ -0,0 +1,15 @@
+package com.britehouse.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Anele on 9/6/2016.
+ */
+public class FactorialTest {
+    @Test
+    public void shouldCalcutaleFactorialOfZero(){
+        assertEquals(1,Factorial.factorial(0));
+    }
+}
---
+++ Factorial/src/com/britehouse/tdd/Factorial.java
@@ -0,0 +1,11 @@
+package com.britehouse.tdd;
+
+/**
+ * Created by Anele on 9/6/2016.
+ */
+public class Factorial {
+
+    public static int factorial(int num) {
+        return 1;
+    }
+}
